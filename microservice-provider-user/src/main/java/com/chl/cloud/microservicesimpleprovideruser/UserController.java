44d90b575d25b755bcc7ec6ff063a960675cfaec
‘chenhailong11’ <196121909@qq.com>
2017-08-03 14:58:25 +0800

    no message

--- microservice-provider-user/src/main/java/com/chl/cloud/microservicesimpleprovideruser/UserController.java
+++ microservice-provider-user/src/main/java/com/chl/cloud/microservicesimpleprovideruser/UserController.java
@@ -1,5 +1,7 @@
 package com.chl.cloud.microservicesimpleprovideruser;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,21 @@ public class UserController {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private EurekaClient discoveryClient;
+
   @GetMapping("/simple/{id}")
   public User findById(@PathVariable Long id) {
+
     return this.userRepository.findOne(id);
   }
+  @GetMapping("/eureka_instance")
+  public String serviceUrl(){
+    InstanceInfo stores = discoveryClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+    return stores.getHomePageUrl();
+  }
+
+
+
+
 }
