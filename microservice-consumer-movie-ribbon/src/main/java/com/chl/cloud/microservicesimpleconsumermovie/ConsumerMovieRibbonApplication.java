c721cebf3ca406f4105fbc66450d75e17d1c1ddf
‘chenhailong11’ <196121909@qq.com>
2017-08-04 11:44:02 +0800

    添加 自定义 ribbon均衡器

--- microservice-consumer-movie-ribbon/src/main/java/com/chl/cloud/microservicesimpleconsumermovie/ConsumerMovieRibbonApplication.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/chl/cloud/microservicesimpleconsumermovie/ConsumerMovieRibbonApplication.java
@@ -1,14 +1,18 @@
 package com.chl.cloud.microservicesimpleconsumermovie;
 
+import com.chl.config.TestConfiguration;
+import com.chl.config.TestRibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "microservice-provider-user",configuration = TestConfiguration.class)
 public class ConsumerMovieRibbonApplication {
 
 	@Bean
