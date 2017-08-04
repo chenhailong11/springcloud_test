c0d3d16ac0e56c349ed6bb85ac85696a6c7aeb96
‘chenhailong11’ <196121909@qq.com>
2017-08-04 13:33:07 +0800

    添加使用配置 配置 ribbon   及feign项目

---
+++ microservice-consumer-movie-ribbon-properties/src/main/java/com/chl/config/TestRibbonConfig.java
@@ -0,0 +1,16 @@
+package com.chl.config;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @version : 1.0
+ * @description
+ * @Date 11:18 2017/8/4
+ * @auth : chenhl
+ */
+
+@Configuration
+public class TestRibbonConfig {
+
+}
